6ab3ffabe4b3de6b494f60f62ee7fa86d7a7d05c
CYX22222003 <e1155533@u.nus.edu>
2026-03-09 15:21:02 +0800

    Add all strategies

--- strategy/__init__.py
+++ strategy/__init__.py
@@ -3,9 +3,9 @@ from strategy.MacdAdxStrategy import MacdAdxStrategy
 from strategy.ONS import ONS_Portfolio
 from strategy.MlpSpeculativeStrategy import MlpSpeculativeStrategy
 strategy_list = [
-    # EmaCrossStrategy.__name__,
-    # MacdAdxStrategy.__name__,
-    # ONS_Portfolio.__name__,
+    EmaCrossStrategy.__name__,
+    MacdAdxStrategy.__name__,
+    ONS_Portfolio.__name__,
     MlpSpeculativeStrategy.__name__
 ]
 
